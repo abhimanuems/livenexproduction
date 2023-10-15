da7ba6437c8b50563a057ed13b2b401123dd1402
Abhimanue <abhimanuems@hotmail.com>
2023-10-16 00:59:33 +0530

    shimmer added

---
+++ client/src/components/admin/Shimmer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const shimmerAnimation = {
+  animation: "shimmer 1.5s infinite",
+  background: "linear-gradient(90deg, #f0f0f0 25%, #e0e0e0 50%, #f0f0f0 75%)",
+  backgroundSize: "200% 100%",
+};
+
+const ShimmerLoader = () => {
+  return (
+    <div className="flex justify-center items-center h-32">
+      <div className="w-32 h-8" style={shimmerAnimation}></div>
+    </div>
+  );
+};
+
+export default ShimmerLoader;
--- client/src/index.js
+++ client/src/index.js
@@ -1,135 +1,117 @@
 import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
-import { Provider, useSelector } from "react-redux";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
 import Loginpage from "./components/user/Login";
 import SignupPage from "./components/user/Signup";
-import store from "./store";
+import store from './store';
 import VideoStreamingPage from "./components/pages/VideoStreamingPage";
 import SubscriptionPage from "./components/pages/SubscriptionPage";
 import VerifiyOtp from "./components/user/VerifiyOtp";
 import ErrorPage from "./components/user/ErrorPage";
 import TicketPage from "./components/pages/TicketPage";
-import Login from "./components/admin/Login";
+import Login from "./components/admin/Login"
 import SharePage from "./components/pages/SharePage";
+import ShimmerLoader from "./components/admin/Shimmer"
 import ErrorPageAdmin from "./components/admin/ErrorPageAdmin";
 const SubscriptionAdmin = lazy(() =>
   import("./components/Adminpages/subscriptionAdmin")
 );
-const UserPageAdmin = lazy(() =>
-  import("./components/Adminpages/UserlistPage")
-);
-const AdminTicketPage = lazy(() =>
-  import("./components/Adminpages/TicketPageAdmin")
-);
-const DashboardPage = lazy(() =>
-  import("./components/Adminpages/DashboardPage")
-);
+const UserPageAdmin = lazy(()=>import("./components/Adminpages/UserlistPage"));
+const AdminTicketPage = lazy(()=>import("./components/Adminpages/TicketPageAdmin"))
+const DashboardPage = lazy(()=>import("./components/Adminpages/DashboardPage"))
 const Homepage = lazy(() => import("./components/pages/Homepage"));
 
-function AppRouter() {
-  const { adminInfo } = useSelector((state) => state.adminAuth);
-  const { userInfo } = useSelector((state) => state.auth);
 
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <App />,
-      errorElement: <ErrorPage />,
-      children: [
-        {
-          path: "/",
-          element: userInfo ? <Homepage /> : <Loginpage />,
-        },
-        {
-          path: "/login",
-          element: <Loginpage />,
-        },
-        {
-          path: "/signup",
-          element: <SignupPage />,
-        },
-        {
-          path: "/video",
-          element: userInfo ? <VideoStreamingPage /> : <Loginpage />,
-        },
-        {
-          path: "/pro",
-          element: userInfo ? <SubscriptionPage /> : <Loginpage />,
-        },
-        {
-          path: "/verifyotp",
-          element: userInfo ? <VerifiyOtp /> : <Loginpage />,
-        },
-        {
-          path: "/ticket",
-          element: userInfo ? <TicketPage /> : <Loginpage />,
-        },
-        {
-          path: "/share",
-          element: userInfo ? <SharePage /> : <Loginpage />,
-        },
-      ],
-    },
-    {
-      path: "/admins",
-      element: <App />,
-      errorElement: <ErrorPageAdmin />,
-      children: [
-        {
-          path: "/admins/dashboard",
-          element: adminInfo ? <DashboardPage /> : <Login />,
-        },
-        {
-          path: "/admins/login",
-          element: <Login />,
-        },
-        {
-          path: "/admins/subscription",
-          element: adminInfo ? (
-            <Suspense fallback={<div>Loading Subscription Admin...</div>}>
-              <SubscriptionAdmin />
-            </Suspense>
-          ) : (
-            <Login />
-          ),
-        },
-        {
-          path: "/admins/user",
-          element: adminInfo ? (
-            <Suspense fallback={<div>Loading Subscription Admin...</div>}>
-              <UserPageAdmin />
-            </Suspense>
-          ) : (
-            <Login />
-          ),
-        },
-        {
-          path: "/admins/ticket",
-          element: adminInfo ? (
-            <Suspense fallback={<div>Loading Subscription Admin...</div>}>
-              <AdminTicketPage />
-            </Suspense>
-          ) : (
-            <Login />
-          ),
-        },
-      ],
-    },
-  ]);
 
-  return (
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
-  );
-}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Homepage />,
+      },
+      {
+        path: "/login",
+        element: <Loginpage />,
+      },
+      {
+        path: "/signup",
+        element: <SignupPage />,
+      },
+      {
+        path: "/video",
+        element: <VideoStreamingPage />,
+      },
+      {
+        path: "/pro",
+        element: <SubscriptionPage />,
+      },
+      {
+        path: "/verifyotp",
+        element: <VerifiyOtp />,
+      },
+      {
+        path: "/ticket",
+        element: <TicketPage />,
+      },
+      {
+        path: "/share",
+        element: <SharePage />,
+      },
+    ],
+  },
+  {
+    path: "/admins",
+    element: <App />,
+    errorElement: <ErrorPageAdmin />,
+    children: [
+      {
+        path: "/admins/dashboard",
+        element: <DashboardPage />,
+      },
+      {
+        path: "/admins/login",
+        element: <Login />,
+      },
+      {
+        path: "/admins/subscription",
+        element: (
+          <Suspense fallback={<ShimmerLoader />}>
+            <SubscriptionAdmin />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/admins/user",
+        element: (
+          <Suspense fallback={<ShimmerLoader />}>
+            <UserPageAdmin />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/admins/ticket",
+        element: (
+          <Suspense fallback={<ShimmerLoader />}>
+            <AdminTicketPage />
+          </Suspense>
+        ),
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppRouter />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
