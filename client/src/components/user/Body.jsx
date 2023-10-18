c0809d60db3e35bcad7651a670cba06bb285c58d
Abhimanue <abhimanuems@hotmail.com>
2023-10-18 18:24:07 +0530

    changes in UI

--- client/src/components/user/Body.jsx
+++ client/src/components/user/Body.jsx
@@ -28,7 +28,6 @@ const Body = () => {
         streamsAPI()
           .unwrap()
           .then((pdata) => {
-            console.log("streaing ddata is ", pdata);
             setData(pdata);
             setHasFetchedData(true);
           })
@@ -61,9 +60,6 @@ const Body = () => {
      return formattedDate;
     }
 
-  
-
-
   return (
     <div className="bg-white w-5/6 p-4">
       <p className="font-semibold text-blue-900 text-2xl p-2 m-2">Streams</p>
@@ -82,23 +78,26 @@ const Body = () => {
       </div>
       <hr />
       {hasFetchedData && data?.response?.length > 0 ? (
+   
         <div>
           <p className="font-semibold text-gray-700 text-lg p-2 m-2">
             Past streams
           </p>
-          <div className="relative overflow-x-auto shadow-l sm:rounded-lg mt-1">
-            <table className="w-3/4 text-sm text-center text-gray-200 dark:text-gray-200">
+          <div className="relative max-h-80 overflow-y-auto scrollbar-hide shadow-l sm:rounded-sm mt-1">
+            <table className="w-3/4 text-sm  text-center text-gray-200 border border-slate-200">
               <thead className="text-xs bg-gray-50 dark:bg-gray-50 dark:text-gray-50">
-                <tr className="bg-gray-100 text-gray-800 text-xs leading-normal">
-                  <th className="px-6 py-3 text-left">Title</th>
-                  <th className="px-6 py-3 text-left">Date</th>
+                <tr className="bg-gray-50 text-gray-800 text-xs leading-normal border border-slate-200">
+                  <th className="px-6 py-3 text-center">Title</th>
+                  <th className="px-6 py-3 text-center">Date</th>
                   <th className="px-6 py-3 text-left">Platforms</th>
                 </tr>
               </thead>
               <tbody>
                 {data?.response?.map((item, index) => (
-                  // {response?.streams.map((item, index) => (
-                  <tr key={index} className="text-gray-600 text-sm font-medium">
+                  <tr
+                    key={index}
+                    className="text-gray-600 text-sm font-medium border border-slate-200"
+                  >
                     {console.log(item)}
                     <td className="px-6 py-4">{item?.title}</td>
                     <td className="px-6 py-4 ">
@@ -106,15 +105,23 @@ const Body = () => {
                         {formatDate(item?.startTime)}
                       </div>
                     </td>
-                    <td className="px-6 py-4">
-                      {item.destinations.youtube === true ? (
-                        <span className="text-red-500">
-                          <BsYoutube />
-                        </span>
-                      ) : item.destinations.facebook === false ? (
-                        <span className="text-blue-500">
-                          <BsFacebook />
-                        </span>
+                    <td className="px-6 py-4 flex text-center">
+                      {item?.destinations[0]?.youtube === true ? (
+                        <BsYoutube
+                          style={{
+                            fontSize: "20px",
+                            color: "red",
+                          }}
+                        />
+                      ) : null}
+                      {item?.destinations[1]?.facebook === true ? (
+                        <BsFacebook
+                          style={{
+                            fontSize: "20px",
+                            color: "blue",
+                            marginLeft: "2rem",
+                          }}
+                        />
                       ) : null}
                     </td>
                   </tr>
